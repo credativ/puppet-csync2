3c45c8adc09eb63d615adaaf4a02b647ee725b56
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2015-01-06 10:25:47 +0100

    Fix params

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,9 +4,8 @@ class csync2::params {
     $ensure_enabled = true
     $manage_config      = true
     $config_file        = '/etc/csync2.cfg'
-    $keyfile            = '/etc/csync2.key':
+    $keyfile            = '/etc/csync2.key'
     $config_template    = undef
     $config_source      = undef
-    $disabled_hosts     = []
 }
 
